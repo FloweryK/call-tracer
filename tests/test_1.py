ef2bf9da1289301f61f1c37acb7e6831b1731b17
FloweryK <flowerk94@gmail.com>
2024-06-15 23:56:45 +0900

    [feat] added tests for stdout, added codes to reset the tracer after test

--- tests/test_1.py
+++ tests/test_1.py
@@ -2,28 +2,61 @@ import os
 from callTracer import tracer
 
 
-def test_tracer():
+def test_tracer(capsys):
     @tracer
-    def sample_function():
+    def function_depth_0():
+        function_depth_1()
+
+    def function_depth_1():
+        function_depth_2()
+
+    def function_depth_2():
         pass
 
-    # manually check
-    sample_function()
+    # capture stdout
+    function_depth_0()
+    captured = capsys.readouterr()
+    assert "  0 CALL" in captured.out
 
 
-def test_set_max_depth():
+def test_set_max_depth(capsys):
     tracer.set_max_depth(1)
     assert tracer.max_depth == 1
+
+    @tracer
+    def function_depth_0():
+        function_depth_1()
+
+    def function_depth_1():
+        function_depth_2()
+
+    def function_depth_2():
+        pass
+
+    # capture stdout
+    function_depth_0()
+    captured = capsys.readouterr()
+    assert "  1 |   CALL" in captured.out
+    assert "  2 |   |   CALL" not in captured.out
+
+    # return to default
+    tracer.set_max_depth(4)
 
 
 def test_set_path_cuts():
     tracer.set_path_cuts(['test_path'])
     assert 'test_path' in tracer.path_cuts
 
+    # return to default
+    tracer.set_path_cuts([])
+
 
 def test_set_path_filters():
     tracer.set_path_filters(['test_path'])
     assert 'test_path' in tracer.path_filters
+
+    # return to default
+    tracer.set_path_filters([])
 
 
 def test_shorten_path():
