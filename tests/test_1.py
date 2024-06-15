04c7d671e4d0450e32c1e1d5601aed209b5b4640
FloweryK <flowerk94@gmail.com>
2024-06-15 22:59:54 +0900

    [feat] added a test for tracer._shorten_path(), cleaned irrelavant codes

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,4 +1,4 @@
-import pytest
+import os
 from callTracer import tracer
 
 
@@ -15,15 +15,6 @@ def test_set_max_depth():
     tracer.set_max_depth(1)
     assert tracer.max_depth == 1
 
-    @tracer
-    def sample_function():
-        def nested_function():
-            pass
-        nested_function()
-
-    # manually check
-    sample_function()
-
 
 def test_set_path_cuts():
     tracer.set_path_cuts(['test_path'])
@@ -35,5 +26,7 @@ def test_set_path_filters():
     assert 'test_path' in tracer.path_filters
 
 
-if __name__ == '__main__':
-    pytest.main()
+def test_shorten_path():
+    t = tracer(func=lambda x: x)
+    t.set_path_cuts(path_cuts=['test_path'])
+    assert 'test_path' not in t._shorten_path(os.path.join('path_1', 'test_path', 'path_2'))
